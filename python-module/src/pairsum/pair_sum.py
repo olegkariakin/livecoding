4f9f62fd97ab94d94c254566614e3871210c3d29
Oleg Kariakin <olegkariakin1988@gmail.com>
2025-05-26 23:25:10 +0300

    Updated "PairSum" Sorted clever 2 pointers for PYTHON module

--- python-module/src/pairsum/pair_sum.py
+++ python-module/src/pairsum/pair_sum.py
@@ -25,8 +25,23 @@ def pair_sum_sorted_brute_force(nums: List[int],
     n = len(nums)
     for i in range(n):
         for j in range(i + 1, n):
-            if nums[i] + nums[j] == target:
+            if nums[i] + nums[j] == tar:
                 return [i, j]
     return []
 
+def pair_sum_sorted(nums: List[int],
+                    tar: int) -> List[int]:
+    """Pair sum with 2 pointers clever algo with O(n) time complexity"""
+    left, right = 0, len(nums) - 1
+    while left < right:
+        clever_sum = nums[left] + nums[right]
+        if clever_sum < tar:
+            left += 1
+        elif clever_sum > tar:
+            right += 1
+        else:
+            return [left, right]
+    return []
+
 print("Result is with brute force approach: ", pair_sum_sorted_brute_force(asc_array, target))
+print("Result is with clever 2 pointer approach: ", pair_sum_sorted(asc_array, target))
