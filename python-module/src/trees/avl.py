ec5426debeffbfa28a7d2b350896c8e10d112778
Oleg Kariakin <olegkariakin1988@gmail.com>
2025-06-01 08:52:49 +0300

    Added Node and some AVL to PYTHON module

--- python-module/src/trees/avl.py
+++ python-module/src/trees/avl.py
@@ -9,3 +9,7 @@ class AVLTree:
             root.left = self.insert(root.left, key)
         else:
             root.right = self.insert(root.right, key)
+
+        # Updating the height
+        root.height = 1 + max(self.get_height(root.left),
+                              self.get_height(root.right))
