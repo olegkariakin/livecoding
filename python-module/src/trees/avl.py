a9a107c9584d149c960a0f9104c233e8c293961d
Oleg Kariakin <olegkariakin1988@gmail.com>
2025-06-01 09:13:15 +0300

    Updating AVL functions for PYTHON module

--- python-module/src/trees/avl.py
+++ python-module/src/trees/avl.py
@@ -13,3 +13,14 @@ class AVLTree:
         # Updating the height
         root.height = 1 + max(self.get_height(root.left),
                               self.get_height(root.right))
+
+        # Check balance and balancing the AVL tree
+        balance = self.get_balance(root)
+
+        # LL case
+        if balance > 1 and key < root.left.key:
+            return self.right_rotate(root)
+
+        # RR case
+        if balance < -1 and key > root.right.key:
+            return self.left_rotate(root)
