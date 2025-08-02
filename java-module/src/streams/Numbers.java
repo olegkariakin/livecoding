d3e46dc364c7f305f60897783d95dbd2ccecc9e6
oleg <olegkariakin1988@gmail.com>
2025-08-02 18:05:29 +0300

    Sum of even and odd int numbers using Stream API

--- java-module/src/streams/Numbers.java
+++ java-module/src/streams/Numbers.java
@@ -9,13 +9,20 @@ public class Numbers {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println("Initial numbers: " + list);
 
-        //Calculate the average using streams
-        double average = list.stream()
-                .mapToDouble(Integer::doubleValue)
-                .average()
-                .orElse(0);
+        //Calculate sumEven of even numbers in the stream
+        int sumEven = list.stream()
+                .filter(d -> d % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
 
-        System.out.println("Average number: " + average);
+        System.out.println("Sum of even number: " + sumEven);
+
+        // Calculate sum of odd numbers
+        int sumOdd = list.stream()
+                .filter(d -> d % 2 != 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println("Sum of odd number: " + sumOdd);
     }
 
 }
