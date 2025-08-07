b74cb255a3484547a433dc8a812de5c1ffa2a8b9
oleg <olegkariakin1988@gmail.com>
2025-08-07 15:12:30 +0300

    Coding: Compress string with minimal space complexity Added one more method to calculate withing a single loop

--- java-module/src/coding/compression/CompressionString.java
+++ java-module/src/coding/compression/CompressionString.java
@@ -3,7 +3,7 @@ package coding.compression;
 // Compress the string "aabcccccaaa" into "a2b1c5a3" with minimal space complexity.
 public class CompressionString {
 
-    public static String compress(String input) {
+    public static String compressEasy(String input) {
         if (input == null || input.length() <= 1) {
             return input;
         }
@@ -29,9 +29,37 @@ public class CompressionString {
         return result.toString();
     }
 
+    public static String compressMedium(String input) {
+        if (input == null || input.length() <= 1) {
+            return input;
+        }
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+        int n = input.length();
+
+        while (i < n) {
+            char currentChar = input.charAt(i);
+            int count = 1;
+
+            // count repetitions
+            while (i + 1 < n && input.charAt(i + 1) == currentChar) {
+                i++;
+                count++;
+            }
+
+            // append current sequence
+            result.append(currentChar);
+            result.append(count);
+            i++;
+        }
+
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         String initial = "aabcccccaaa";
         System.out.println("Initial: " + initial);
-        System.out.println("Compressed: " + compress(initial));
+        System.out.println("Compressed: " + compressMedium(initial));
     }
 }
