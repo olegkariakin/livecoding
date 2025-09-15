9d365e1f52b2ad6df16232c7bad770617059ce55
oleg <olegkariakin1988@gmail.com>
2025-09-15 13:45:41 +0300

    Simple executor service example

---
+++ java-module/src/concurrent/SimpleExec.java
@@ -0,0 +1,54 @@
+package concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleExec {
+
+    static void main() {
+        CountDownLatch cdl1 = new CountDownLatch(5);
+        CountDownLatch cdl2 = new CountDownLatch(5);
+        CountDownLatch cdl3 = new CountDownLatch(5);
+        CountDownLatch cdl4 = new CountDownLatch(5);
+        ExecutorService es = Executors.newFixedThreadPool(2);
+            System.out.println("Launch");
+
+            // Thread start
+            es.execute(new MyThread5("A", cdl1));
+            es.execute(new MyThread5("B", cdl2));
+            es.execute(new MyThread5("C", cdl3));
+            es.execute(new MyThread5("D", cdl4));
+
+        try {
+            cdl1.await();
+            cdl2.await();
+            cdl3.await();
+            cdl4.await();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        es.shutdown();
+        System.out.println("Accomplishment");
+    }
+}
+
+class MyThread5 implements Runnable {
+    String name;
+    CountDownLatch countDownLatch;
+
+    MyThread5(String name, CountDownLatch countDownLatch) {
+        this.name = name;
+        this.countDownLatch = countDownLatch;
+        new Thread(this);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + ": " + i);
+            countDownLatch.countDown();
+        }
+    }
+}
