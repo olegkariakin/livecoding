b5b95d46f4cd96f0d606f7869b351138d1c6fc5a
oleg <olegkariakin1988@gmail.com>
2026-01-12 15:28:29 +0200

    Binary Search recursive implementation

--- java-module/src/search/BinarySearch.java
+++ java-module/src/search/BinarySearch.java
@@ -10,7 +10,7 @@ public class BinarySearch {
         System.out.print("Sorted array: ");
         Arrays.stream(values).forEach(a -> System.out.print(STR."\{a} "));
         int key = 3;
-        System.out.println(STR."\nKey: \{key} found on position: \{binarySearch(values, key)}");
+        System.out.println(STR."\nKey: \{key} found on position: \{binarySearchRecursive(values, 0, values.length - 1, key)}");
     }
 
     static int binarySearch(int[] values, int key) {
@@ -30,5 +30,20 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    static int binarySearchRecursive(int[] array, int left, int right, int key) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+        if (array[mid] == key) {
+            return mid;
+        }
+
+        if (key > array[mid]) {
+            return binarySearchRecursive(array, mid + 1, right, key);
+        }
+        return binarySearchRecursive(array, left, mid - 1, key);
     }
 }
