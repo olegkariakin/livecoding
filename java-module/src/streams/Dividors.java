cc0de5bb4061912a0eb645f085df8d0ef540b106
oleg <olegkariakin1988@gmail.com>
2026-01-30 14:21:40 +0200

    Find 2 and 7 dividors using Stream API calculate statistics of count and sum.

--- java-module/src/streams/Dividors.java
+++ java-module/src/streams/Dividors.java
@@ -1,5 +1,6 @@
 package streams;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -8,6 +9,8 @@ import java.util.stream.IntStream;
 public class Dividors {
     static void main() {
         calc(15);
+        List<Integer> result = calcStatistics(15);
+        System.out.println("Statistics. Count: " + result.get(0) + " Sum: " + result.get(1));
     }
 
     static void calc(int max) {
@@ -18,7 +21,24 @@ public class Dividors {
                     if (i % 2 == 0) return "Делится на 2";
                     return "Делится на 7";
                 }));
-        groups.forEach((key, list) -> System.out.println(key + ": " + list));
+        groups.forEach((key, list) -> System.out.println(STR."\{key}: \{list}"));
+    }
+
+    static List<Integer> calcStatistics(int max) {
+        List<Integer> results = new ArrayList<>();
+
+        int count = (int) IntStream.rangeClosed(2, max)
+                .filter(i -> i % 2 == 0 || i % 7 == 0)
+                .count();
+
+        int sum = IntStream.rangeClosed(2, max)
+                .filter(i -> i % 2 == 0 || i % 7 == 0)
+                .sum();
+
+        results.add(count);
+        results.add(sum);
+
+        return results;
     }
 
 }
