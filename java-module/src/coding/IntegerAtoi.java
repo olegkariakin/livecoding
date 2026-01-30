2fcda516be274a6bf180c6043360065b8c50255c
oleg <olegkariakin1988@gmail.com>
2026-01-30 20:49:05 +0200

    String to Integer ATOI (medium leetcode 8 https://leetcode.com/problems/string-to-integer-atoi/description/)

--- java-module/src/coding/IntegerAtoi.java
+++ java-module/src/coding/IntegerAtoi.java
@@ -10,26 +10,31 @@ public class IntegerAtoi {
         int n = s.length();
         int i = 0;
         int sign = 1;
-        long res = 0;
+        int res = 0;
 
         // skip spaces
         while (i < n && s.charAt(i) == ' ') i++;
 
-        // identify + or - sign
+        // check sign, if -+-+- it'll exist on 2nd element
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
             sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
 
+        // reading digits and forming a number, if leading +-+-+-+234234 this loop won't start cause '-' isn't digit
         while (i < n && Character.isDigit(s.charAt(i))) {
-            res = res * 10 + (s.charAt(i) - '0');
+            int digit = (s.charAt(i) - '0');
+            // Checking result overflow
+            if (sign == 1 && (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && digit > 7))) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && (-res < Integer.MIN_VALUE / 10 || (-res == Integer.MIN_VALUE / 10 && digit > 8))) {
+                return Integer.MIN_VALUE;
+            }
+            res = res * 10 + digit;
             i++;
-
-            // check overflow using long
-            if (sign == 1 && res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (sign == -1 && -res < Integer.MIN_VALUE) return Integer.MIN_VALUE;
         }
 
-        return (int) (res * sign);
+        return res * sign;
     }
 }
