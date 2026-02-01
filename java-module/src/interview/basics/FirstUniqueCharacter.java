7d96589509dca72a1ec378c3c6260b03a06a3e22
oleg <olegkariakin1988@gmail.com>
2026-02-01 19:40:31 +0200

    First Unique character in a string using array for ASCII: O(n) complexity and O(n) memory

--- java-module/src/interview/basics/FirstUniqueCharacter.java
+++ java-module/src/interview/basics/FirstUniqueCharacter.java
@@ -7,7 +7,7 @@ public class FirstUniqueCharacter {
 
     static void main() {
         String s = "lleettccoode";
-        char result = findFirstUniqueMap(s);
+        char result = findFirstUniqueArray(s);
         System.out.println(result != '\0' ? result : "No unique chars");
     }
 
@@ -30,6 +30,28 @@ public class FirstUniqueCharacter {
         }
         for (char c : input.toCharArray()) {
             if (map.get(c) == 1) return c;
+        }
+        return '\0';
+    }
+
+    // in case of ASCII chars only used 256 array
+    // time complexity O(n) space complexity O (n)
+    // array consumes less than a map
+    static char findFirstUniqueArray(String input) {
+        int[] freqs = new int[256];
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c < 256) {
+                freqs[c]++;
+            }
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c < 256 && freqs[c] == 1) {
+                return c;
+            }
         }
         return '\0';
     }
