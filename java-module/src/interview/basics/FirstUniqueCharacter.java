cd0c01c0f22dc18d586d550a454745c2c68d9a9f
oleg <olegkariakin1988@gmail.com>
2026-02-01 19:35:34 +0200

    First Unique character in a string using map: O(n) complexity and O(n) memory

--- java-module/src/interview/basics/FirstUniqueCharacter.java
+++ java-module/src/interview/basics/FirstUniqueCharacter.java
@@ -1,10 +1,13 @@
 package interview.basics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstUniqueCharacter {
 
     static void main() {
         String s = "lleettccoode";
-        char result = findFirstUniqueTwoPointers(s);
+        char result = findFirstUniqueMap(s);
         System.out.println(result != '\0' ? result : "No unique chars");
     }
 
@@ -15,6 +18,18 @@ public class FirstUniqueCharacter {
             if (input.indexOf(c) == input.lastIndexOf(c)) {
                 return c;
             }
+        }
+        return '\0';
+    }
+
+    // O(n) complexity, O(n) memory 3xN, map, 2 implicit char[] arrays of initial string
+    static char findFirstUniqueMap(String input) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : input.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : input.toCharArray()) {
+            if (map.get(c) == 1) return c;
         }
         return '\0';
     }
