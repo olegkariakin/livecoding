036ef54b855c9b05dce51cc493b555f38b9fc59e
oleg <olegkariakin1988@gmail.com>
2026-02-01 19:22:40 +0200

    Reverse words in a string

--- java-module/src/interview/basics/ReverseStringWords.java
+++ java-module/src/interview/basics/ReverseStringWords.java
@@ -1,4 +1,19 @@
 package interview.basics;
 
 public class ReverseStringWords {
+
+    static void main() {
+        String initial = " Some words here";
+        System.out.println(STR."Reverse words: \{initial} - \{reversedWords(initial)}");
+    }
+
+    static String reversedWords(String initial) {
+        StringBuilder reversed = new StringBuilder();
+        String[] words = initial.split(" ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i != 0) reversed.append(" ");
+        }
+        return reversed.toString();
+    }
 }
