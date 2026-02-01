fd38d605b1271531ce3e6f175bc0e610e251f585
oleg <olegkariakin1988@gmail.com>
2026-02-01 19:31:30 +0200

    First Unique character in a string with O(n^2) complexity and O(1) memory

--- java-module/src/interview/basics/FirstUniqueCharacter.java
+++ java-module/src/interview/basics/FirstUniqueCharacter.java
@@ -1,4 +1,21 @@
 package interview.basics;
 
 public class FirstUniqueCharacter {
+
+    static void main() {
+        String s = "lleettccoode";
+        char result = findFirstUniqueTwoPointers(s);
+        System.out.println(result != '\0' ? result : "No unique chars");
+    }
+
+    // O(n^2) complexity, memory O(1)
+    static char findFirstUniqueTwoPointers(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (input.indexOf(c) == input.lastIndexOf(c)) {
+                return c;
+            }
+        }
+        return '\0';
+    }
 }
