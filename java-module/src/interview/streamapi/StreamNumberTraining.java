5f7bacc79033fe012c98e93cf28d92547ef0d060
oleg <olegkariakin1988@gmail.com>
2026-02-02 19:35:24 +0200

    StreamAPI number ZigZag using StreamAPI

--- java-module/src/interview/streamapi/StreamNumberTraining.java
+++ java-module/src/interview/streamapi/StreamNumberTraining.java
@@ -1,6 +1,7 @@
 package interview.streamapi;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -20,20 +21,38 @@ public class StreamNumberTraining {
         IntStream.iterate(2, n -> n + 1)
                 .filter(number -> number > 1 && IntStream.rangeClosed(2, (int) Math.sqrt(number))
                         .noneMatch(i -> number % i == 0))
-                .limit(100)
+                .limit(10)
                 .forEach(System.out::println);
 
         //3. Пифагоровы тройки
         System.out.println("3. Pythagorean triangles");
-        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100)
+        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 50)
                 .boxed()
-                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                .flatMap(a -> IntStream.rangeClosed(a, 50)
                         .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
                         .mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)})
                 );
         pythagoreanTriples
                 .map(Arrays::toString)
-                .forEach(System.out::print);
+                .forEach(System.out::println);
+
+        //4. ZigZag stream
+        String zigZagResult = IntStream.rangeClosed(1, 50)
+                .filter(n -> n % 3 == 0 || n % 5 == 0)
+                .boxed()
+                .sorted((a, b) -> {
+                    boolean aIsEven = a % 2 == 0;
+                    boolean bIsEven = b % 2 == 0;
+
+                    if (aIsEven != bIsEven) {
+                        return aIsEven ? 1 : -1;
+                    }
+                    return aIsEven ? b.compareTo(a) : a.compareTo(b);
+                })
+                .skip(3)
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+        System.out.println("4. ZigZag result: " + zigZagResult);
     }
 
 }
